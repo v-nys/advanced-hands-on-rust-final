b02cf68186cecc116b29ffa3e9858f2e0725303b
Vincent Nys <vincentnys@gmail.com>
2025-10-19 17:29:24 +0200

    Should be working Pig.

--- pig/src/main.rs
+++ pig/src/main.rs
@@ -2,7 +2,6 @@ use bevy::prelude::*;
 use bevy_egui::{EguiContexts, EguiPlugin, egui};
 use my_library::RandomNumberGenerator;
 
-// what is States?
 #[derive(Clone, Copy, PartialEq, Eq, Debug, Hash, Default, States)]
 enum GamePhase {
     #[default]
@@ -79,17 +78,104 @@ fn spawn_die(
     ));
 }
 
+fn clear_die(hand_query: &Query<(Entity, &Sprite), With<HandDie>>, commands: &mut Commands) {
+    hand_query
+        .iter()
+        .for_each(|(entity, _)| commands.entity(entity).despawn());
+}
+
+fn player(
+    hand_query: Query<(Entity, &Sprite), With<HandDie>>,
+    mut commands: Commands,
+    mut rng: ResMut<Random>,
+    assets: Res<GameAssets>,
+    mut scores: ResMut<Scores>,
+    mut state: ResMut<NextState<GamePhase>>,
+    mut egui_context: EguiContexts,
+) {
+    egui::Window::new("Play Options").show(egui_context.ctx_mut(), |ui| {
+        let hand_score: usize = hand_query
+            .iter()
+            .map(|(_, ts)| ts.texture_atlas.as_ref().unwrap().index + 1)
+            .sum();
+        ui.label(&format!("Score for this hand: {hand_score}"));
+        if ui.button("Roll Dice").clicked() {
+            let new_roll = rng.0.range(1..7);
+            if new_roll == 1 {
+                clear_die(&hand_query, &mut commands);
+                state.set(GamePhase::Cpu);
+            } else {
+                spawn_die(
+                    &hand_query,
+                    &mut commands,
+                    &assets,
+                    new_roll as usize,
+                    Color::WHITE,
+                );
+            }
+        }
+        if ui.button("Pass - Keep Hand Score").clicked() {
+            let hand_total: usize = hand_query
+                .iter()
+                .map(|(_, ts)| ts.texture_atlas.as_ref().unwrap().index + 1)
+                .sum();
+            scores.player += hand_total;
+            clear_die(&hand_query, &mut commands);
+            state.set(GamePhase::Cpu);
+        }
+    });
+}
+
+#[allow(clippy::too_many_arguments)]
+fn cpu(
+    hand_query: Query<(Entity, &Sprite), With<HandDie>>,
+    mut state: ResMut<NextState<GamePhase>>,
+    scores: Res<Scores>,
+    mut rng: ResMut<Random>,
+    mut commands: Commands,
+    assets: Res<GameAssets>,
+    mut timer: ResMut<HandTimer>,
+    time: Res<Time>,
+) {
+    timer.0.tick(time.delta());
+    if timer.0.just_finished() {
+        let hand_total: usize = hand_query
+            .iter()
+            .map(|(_, ts)| ts.texture_atlas.as_ref().unwrap().index + 1)
+            .sum();
+        if hand_total < 20 && scores.cpu + hand_total < 100 {
+            let new_roll = rng.0.range(1..7);
+            if new_roll == 1 {
+                clear_die(&hand_query, &mut commands);
+                state.set(GamePhase::Player);
+            } else {
+                spawn_die(
+                    &hand_query,
+                    &mut commands,
+                    &assets,
+                    new_roll as usize,
+                    Color::Srgba(Srgba::new(0.0, 0.0, 1.0, 1.0)),
+                );
+            }
+        }
+    }
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(EguiPlugin {
-            // ??
             enable_multipass_for_primary_context: false,
         })
         .add_systems(Startup, setup)
         .init_state::<GamePhase>()
-        .add_systems(Update, display_score)
-        .add_systems(Update, player.run_if(in_state(GamePhase::Player)))
-        .add_systems(Update, cpu.run_if(in_state(GamePhase::Cpu)))
+        .add_systems(
+            Update,
+            (
+                display_score,
+                player.run_if(in_state(GamePhase::Player)),
+                cpu.run_if(in_state(GamePhase::Cpu)),
+            ),
+        )
         .run();
 }
