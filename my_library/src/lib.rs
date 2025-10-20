0173854c7d4ae8b4e9801f4fad144a2a5ffd421d
Vincent Nys <vincentnys@gmail.com>
2025-10-20 15:17:29 +0200

    Complete chapter 4.

--- my_library/src/lib.rs
+++ my_library/src/lib.rs
@@ -1,6 +1,14 @@
 pub use rand;
 
+#[cfg(not(feature = "locking"))]
+mod random;
+
+#[cfg(not(feature = "locking"))]
+pub use random::*;
+
+#[cfg(feature = "locking")]
 mod random_locking;
+#[cfg(feature = "locking")]
 pub use random_locking::*;
 
 pub fn add(left: u64, right: u64) -> u64 {
